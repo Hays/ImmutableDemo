05b5f65b039571d160ff1a0b53882bbb8facd348
Hays <huangwenxi@cvte.com>
2019-05-08 19:23:27 +0800

    [modify]update demo

--- App.js
+++ App.js
@@ -22,9 +22,12 @@ export default class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
     this._run = this._run.bind(this)
+    this._run2 = this._run2.bind(this)
     this.state = {
       timeElapsed: 0,
       timeElapsed2: 0,
+      timeElapsed3: 0,
+      timeElapsed4: 0,
       loading: false
     }
   }
@@ -48,12 +51,39 @@ export default class App extends Component<Props, State> {
       }, 3000)
     })
   }
+  _run2() {
+    const members = mock.members
+    const now = new Date()
+    const member = members.find(m => m.user._id === '64f5e4b5-c106-4076-824a-3b826972e999')
+    console.log('>>> test by Hays 1. ', member)
+    const after = new Date()
+    const timeElapsed3 = after.getTime() - now.getTime()
+    this.setState({
+      timeElapsed3
+    }, () => {
+      setTimeout(() => {
+        const data = fromJS(mock)
+        const begin = new Date()
+        const list = data.get('members')
+        const find = list.find(m => m.getIn(['user', '_id']) === '64f5e4b5-c106-4076-824a-3b826972e999')
+        console.log('>>> test by Hays 2. ', find)
+        const end = new Date()
+        const timeElapsed4 = end.getTime() - begin.getTime()
+        this.setState({
+          timeElapsed4
+        })
+      }, 3000)
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>{`fromJS run time elapsed: ${this.state.timeElapsed} ms`}</Text>
         <Text>{`toJS run time elapsed: ${this.state.timeElapsed2} ms`}</Text>
-        <Button title='Run' onPress={this._run} />
+        <Button title='Run1' onPress={this._run} />
+        <Text>{`Array find time elapsed: ${this.state.timeElapsed3} ms`}</Text>
+        <Text>{`List find time elapsed: ${this.state.timeElapsed4} ms`}</Text>
+        <Button title='Run2' onPress={this._run2} />
       </View>
     );
   }
